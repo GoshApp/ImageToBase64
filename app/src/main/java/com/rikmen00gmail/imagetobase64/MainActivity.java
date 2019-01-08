c1fa42ee5e5116484f91b040f52c4bb77fd83f2b
Rikmen <rikmen00@gmail.com>
2019-01-08 13:58:12 +0300

    Added selection of galleries and setting.

--- app/src/main/java/com/rikmen00gmail/imagetobase64/MainActivity.java
+++ app/src/main/java/com/rikmen00gmail/imagetobase64/MainActivity.java
@@ -1,17 +1,23 @@
 package com.rikmen00gmail.imagetobase64;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RESULT_LOAD_IMG = 1;
     private ImageView imageView2;
     private EditText textView;
     @Override
@@ -36,6 +42,38 @@ public class MainActivity extends AppCompatActivity {
         imageByteArray = Base64.decode(img_str, Base64.DEFAULT);
         Bitmap decodedImage = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.length);
         imageView2.setImageBitmap(decodedImage);
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //Clear the Activity's bundle of the subsidiary fragments' bundles.
+        outState.clear();
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                imageView2.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        }else {
+            Toast.makeText(MainActivity.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
